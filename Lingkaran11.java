81ca64e0610313d6ba4787ab41a83a4d1d4a092d
dhevinaagustina <dhevinaagustina@gmail.com>
2023-09-18 09:48:36 +0700

    percobaan 1

---
+++ Lingkaran11.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Lingkaran11 {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+
+        int r;
+        double keliling, luas;
+
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        r=sc.nextInt();
+
+        keliling= 2*3.14*r;
+        luas= 3.14*r*r;
+
+        System.out.print("Keliling lingkaran adalah:");
+        System.out.println(keliling);
+        
+        System.out.print("Luas lingkaran adalah:");
+        System.out.println(luas);
+        System.out.println(2+1);
+
+
+
+
+
+
+
+    }
+}
