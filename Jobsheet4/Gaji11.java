b2a45c2560df356041b64886784b9a97ae77b5bc
dhevinaagustina <dhevinaagustina@gmail.com>
2023-09-22 20:03:20 +0700

    revisi

--- Jobsheet4/Gaji11.java
+++ Jobsheet4/Gaji11.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
 public class Gaji11{
     public static void main(String[] args) {
-        
-    Scanner sc =new Scanner(System.in);
-    int jmlMasuk, jmlTdkMasuk, totgaji;
-    int gaji=40000, potGaji=25000;
 
+    //Deklarasi
+    Scanner sc =new Scanner(System.in);
+    int jmlMasuk, jmlTdkMasuk, totGaji;
+    int gaji , potGaji;
+
+    //Input
     System.out.print("Masukkan Jumlah Hari Masuk Kerja Anda: ");
     jmlMasuk=sc.nextInt();
 
     System.out.print("Masukkan Jumlah Hari Tidak Masuk Kerja Anda: ");
     jmlTdkMasuk=sc.nextInt();
 
-    totgaji=(jmlMasuk*gaji)-(jmlTdkMasuk*potGaji);
-    System.out.println("Total gaji:  " +totgaji );
-    
+    System.out.println("Besaran Gaji:");
+    gaji=sc.nextInt();
+
+    System.out.println("Potongan Gaji: ");
+    potGaji=sc.nextInt();
+
+    //Operator
+    totGaji=(jmlMasuk*gaji)-(jmlTdkMasuk*potGaji);
+
+    //Output
+    System.out.println("Gaji yang anda terima adalah:  " +totGaji );
+    sc.close();
     }
 }
 
