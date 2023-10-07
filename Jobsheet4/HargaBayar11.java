1ece9ae9d56b479a54490f2492cb230a295fa551
dhevinaagustina <dhevinaagustina@gmail.com>
2023-10-07 16:37:43 +0700

    commit

--- Jobsheet4/HargaBayar11.java
+++ Jobsheet4/HargaBayar11.java
@@ -5,27 +5,35 @@ public class HargaBayar11 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         
+        //Deklarasi
         int harga, jumlah, jmlHalaman;
-        double dis=0.1, total, bayar, jmlDis, besaranDis;
+        double dis=0.1, total, bayar, jmlDis;
         String merkBuku;
         
-        System.out.print("Masukkan harga barang yang dibeli: ");
+        //Input
+        System.out.print("Masukkan harga barang yang dibeli\t\t:" +"Rp.");
         harga=sc.nextInt();
-        System.out.print("Masukkan jumlah-jumlah barang yang akan dibeli: ");
+        System.out.print("Masukkan jumlah-jumlah barang yang akan dibeli\t:");
         jumlah=sc.nextInt();
-        System.out.println("Masukkan merk buku yang anda pilih: ");
+        System.out.print("Masukkan merk buku yang anda pilih\t\t:");
         merkBuku=sc.next();
-        System.out.print("Masukkan jumlah halaman buku: ");
+        System.out.print("Masukkan jumlah halaman buku\t\t\t:");
         jmlHalaman=sc.nextInt();
+        System.out.print("Masukkan diskon yang anda dapat (Desimal)\t:");
+        dis = sc.nextDouble();
+    
 
-
-
+        //Operator
         total=harga*jumlah;
         jmlDis=total*dis;
         bayar=total-jmlDis;
 
-        System.out.println("Diskon yang anda dapatkan adalah " +jmlDis);
-        System.out.println("Jumlah yang harus dibayar adalah " +bayar);
+        //Output
+        System.out.println("Jumlah halaman buku adalah\t\t\t:" + jmlHalaman);
+        System.out.println("Merk Buku\t\t\t\t\t:" + merkBuku);
+        System.out.println("Diskon yang anda dapatkan adalah\t\t:" +jmlDis);
+        System.out.println("Jumlah yang harus dibayar adalah\t\t:" +bayar);
 
+        sc.close();
     }
 }
--- Jobsheet4/Lingkaran11.java
+++ Jobsheet4/Lingkaran11.java
@@ -5,19 +5,23 @@ public class Lingkaran11 {
     public static void main(String[] args) {
         Scanner sc =new Scanner (System.in);
 
+        //Deklarasi
         int r;
-        double keliling, luas;
+        double keliling, luas, phi=3.14;
 
-        System.out.print("Masukkan jari-jari lingkaran: ");
+        //Input
+        System.out.print("Masukkan jari-jari lingkaran\t:");
         r=sc.nextInt();
 
-        keliling= 2*3.14*r;
-        luas= 3.14*r*r;
+        //Operator
+        keliling= 2*phi*r;
+        luas= phi*r*r;
 
-        System.out.print("Keliling lingkaran adalah:");
+        //Output
+        System.out.print("Keliling lingkaran adalah\t:");
         System.out.println(keliling);
         
-        System.out.print("Luas lingkaran adalah:");
+        System.out.print("Luas lingkaran adalah\t\t:");
         System.out.println(luas);
 
 
@@ -26,6 +30,6 @@ public class Lingkaran11 {
 
 
 
-
+        sc.close();
     }
 }
