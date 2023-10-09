d6a847e910e591e9ba7ff74f3e5bf1412b0772a3
dhevinaagustina <dhevinaagustina@gmail.com>
2023-10-09 23:13:45 +0700

    commit

--- ForKelipatan10.java
+++ ForKelipatan10.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ForKelipatan {
+public class ForKelipatan10 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
          
