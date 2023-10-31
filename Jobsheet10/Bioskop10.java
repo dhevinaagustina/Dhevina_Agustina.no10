8a7f3dd74a1ae04581602f8f5655aa837e5caab8
dhevinaagustina <dhevinaagustina@gmail.com>
2023-10-31 10:25:35 +0700

    commit

---
+++ Jobsheet10/Bioskop10.java
@@ -0,0 +1,56 @@
+package Jobsheet10;
+
+public class Bioskop10 {
+    public static void main(String[] args) {
+        String [][]penonton = new String [4][2];
+
+        penonton [0][0] = "Amin";
+        penonton [0][1] = "Bena";
+        penonton [1][0] = "Candra";
+        penonton [1][1] = "Dela";
+        penonton [2][0] = "Eka";
+        penonton [2][1] = "Farhan";
+        penonton [3][0] = "Gisel";
+        penonton [3][1] = "Hana";
+
+        System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+
+        System.out.println("Panjang Baris: " + penonton.length);
+        //System.out.println(penonton[0].length);
+        //System.out.println(penonton[1].length);
+        //System.out.println(penonton[2].length);
+        //System.out.println(penonton[3].length);
+    
+        //for (int i = 0; i < penonton.length; i++){
+        //    System.out.println("Panjang baris ke- " + (i+1) + ": " + penonton[i].length );
+        //}
+        
+        for (String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        
+         System.out.println("Penonton pada baris ke-3: ");
+       // for (int i = 0; i < penonton[2].length; i++){
+       //     System.out.println(penonton[2][i]);
+       // }
+
+       // for (String i : penonton[2]){
+       //     System.out.println(i);
+       // }
+
+       for (int i = 0; i < penonton.length; i++){
+        System.out.println("Penonton pada baris ke- " + (i+1) + ": " + String.join(",", penonton[i]));
+       }
+
+
+
+
+
+
+
+    }
+    
+}
